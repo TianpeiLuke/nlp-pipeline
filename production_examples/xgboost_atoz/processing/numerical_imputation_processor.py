21e332589983a1215e4b3081f75d0662a6b8e943
TianpeiLuke <tianpei@umich.edu>
2025-06-12 20:17:05 -0700

    imputation

--- production_examples/xgboost_atoz/processing/numerical_imputation_processor.py
+++ production_examples/xgboost_atoz/processing/numerical_imputation_processor.py
@@ -5,7 +5,7 @@ import json
 import logging
 from pathlib import Path
 
-from .processors import Processor
+from .processors import Processor, ComposedProcessor
 
 # Setup logger
 logger = logging.getLogger(__name__)
